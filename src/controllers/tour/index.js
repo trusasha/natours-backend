ee330b1e96554b3ac5ee1ac657ecbd3a599ce990
trusasha <hts.sasha800i@gmail.com>
2023-12-20 20:13:45 +0100

    feat: added api-features util

--- src/controllers/tour/index.js
+++ src/controllers/tour/index.js
@@ -1,4 +1,5 @@
 const Tour = require("../../models/tour");
+const ApiFeatures = require("../../utils/api-features");
 
 const throwErrorMessage = (res, status, message) => {
   console.error("Error: ", message);
@@ -7,14 +8,6 @@ const throwErrorMessage = (res, status, message) => {
     status: "error",
     message,
   });
-};
-
-const filterQuery = (req, excludedFields) => {
-  const query = { ...req.query };
-
-  excludedFields.forEach((key) => delete query[key]);
-
-  return query;
 };
 
 const aliasTopTours = async (req, res, next) => {
@@ -27,46 +20,13 @@ const aliasTopTours = async (req, res, next) => {
 
 const getAllTours = async (req, res) => {
   try {
-    const query = filterQuery(req, ["page", "sort", "limit", "fields"]);
+    const features = new ApiFeatures(Tour.find(), req.query)
+      .limitFields()
+      .pagination()
+      .filter()
+      .sort();
 
-    const queryWithAttributes = JSON.parse(
-      JSON.stringify(query).replace(
-        /\b(gte|gt|lte|lt)\b/g,
-        (match) => `$${match}`,
-      ),
-    );
-
-    let request = Tour.find(queryWithAttributes);
-
-    if (req.query.sort) {
-      request = request.sort(req.query.sort.replaceAll(",", " "));
-    } else {
-      request = request.sort("-createdAt");
-    }
-
-    if (req.query.fields) {
-      const fields = req.query.fields.replaceAll(",", " ");
-
-      request = request.select(fields);
-    } else {
-      request = request.select("-__v");
-    }
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    request = request.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-
-      if (skip >= numTours) {
-        throw new Error("This page is not exist");
-      }
-    }
-
-    const tours = await request;
+    const tours = await features.query;
 
     res.status(200).json({
       status: "success",
