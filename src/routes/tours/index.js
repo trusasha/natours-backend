a2c0f6f3144331d9328df2f4220c9cf580d9d1b2
trusasha <hts.sasha800i@gmail.com>
2024-01-02 20:59:49 +0100

    feat: added get monthly stats endpoint

--- src/routes/tours/index.js
+++ src/routes/tours/index.js
@@ -8,12 +8,14 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require("../../controllers/tour");
 
 const router = express.Router();
 
 router.route("/tour-stats").get(getTourStats);
 router.route("/top-5-tours").get(aliasTopTours, getAllTours);
+router.route("/get-monthly-plan/:year").get(getMonthlyPlan);
 
 router.route("/").get(getAllTours).post(addTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
